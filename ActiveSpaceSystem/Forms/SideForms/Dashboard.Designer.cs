9f9a062009b46bc572291917b07b4551cdbd9c3f
POWER LAP <POWER LAP@DESKTOP-RSDGTD1>
2026-04-13 13:05:35 +0200

    dashboard1

---
+++ ActiveSpaceSystem/Forms/SideForms/Dashboard.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace ActiveSpaceSystem.Forms.SideForms
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dashboard));
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            pictureBox2 = new PictureBox();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            pictureBox3 = new PictureBox();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            panel4 = new Panel();
+            pictureBox4 = new PictureBox();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(559, 62);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(369, 150);
+            panel1.TabIndex = 0;
+            panel1.Paint += panel1_Paint;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(22, 17);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(70, 70);
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Tajawal", 11.999999F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.Green;
+            label3.Location = new Point(285, 103);
+            label3.Name = "label3";
+            label3.Size = new Size(70, 29);
+            label3.TabIndex = 2;
+            label3.Text = "+8% ↗";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Tajawal", 16.1999989F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(259, 50);
+            label2.Name = "label2";
+            label2.Size = new Size(45, 40);
+            label2.TabIndex = 1;
+            label2.Text = "24";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Tajawal Medium", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Gray;
+            label1.Location = new Point(172, 17);
+            label1.Name = "label1";
+            label1.Size = new Size(194, 23);
+            label1.TabIndex = 0;
+            label1.Text = "إجمالي الحجوزات اليوم ";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(22, 17);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(70, 70);
+            pictureBox2.TabIndex = 3;
+            pictureBox2.TabStop = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Tajawal", 11.999999F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.Green;
+            label4.Location = new Point(278, 103);
+            label4.Name = "label4";
+            label4.Size = new Size(70, 29);
+            label4.TabIndex = 2;
+            label4.Text = "+8% ↗";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Tajawal", 16.1999989F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(224, 50);
+            label5.Name = "label5";
+            label5.RightToLeft = RightToLeft.Yes;
+            label5.Size = new Size(124, 40);
+            label5.TabIndex = 1;
+            label5.Text = "3000 د.ل";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Tajawal Medium", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.Gray;
+            label6.Location = new Point(165, 17);
+            label6.Name = "label6";
+            label6.Size = new Size(187, 23);
+            label6.TabIndex = 0;
+            label6.Text = "إجمالي الإيرادات اليوم ";
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.White;
+            panel2.Controls.Add(pictureBox2);
+            panel2.Controls.Add(label4);
+            panel2.Controls.Add(label5);
+            panel2.Controls.Add(label6);
+            panel2.Location = new Point(95, 62);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(369, 150);
+            panel2.TabIndex = 4;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.White;
+            panel3.Controls.Add(pictureBox3);
+            panel3.Controls.Add(label7);
+            panel3.Controls.Add(label8);
+            panel3.Controls.Add(label9);
+            panel3.Location = new Point(559, 257);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(369, 150);
+            panel3.TabIndex = 4;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(22, 17);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(70, 70);
+            pictureBox3.TabIndex = 3;
+            pictureBox3.TabStop = false;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Tajawal", 11.999999F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.ForeColor = Color.Green;
+            label7.Location = new Point(285, 103);
+            label7.Name = "label7";
+            label7.Size = new Size(78, 29);
+            label7.TabIndex = 2;
+            label7.Text = "+25% ↗";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Tajawal", 16.1999989F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.Location = new Point(259, 50);
+            label8.Name = "label8";
+            label8.Size = new Size(55, 40);
+            label8.TabIndex = 1;
+            label8.Text = "150";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Tajawal Medium", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label9.ForeColor = Color.Gray;
+            label9.Location = new Point(172, 17);
+            label9.Name = "label9";
+            label9.Size = new Size(184, 23);
+            label9.TabIndex = 0;
+            label9.Text = "عدد العملاء النشطين";
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.White;
+            panel4.Controls.Add(pictureBox4);
+            panel4.Controls.Add(label10);
+            panel4.Controls.Add(label11);
+            panel4.Controls.Add(label12);
+            panel4.Location = new Point(95, 257);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(369, 150);
+            panel4.TabIndex = 4;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(22, 17);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(70, 70);
+            pictureBox4.TabIndex = 3;
+            pictureBox4.TabStop = false;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Tajawal", 11.999999F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label10.ForeColor = Color.Crimson;
+            label10.Location = new Point(285, 103);
+            label10.Name = "label10";
+            label10.Size = new Size(59, 29);
+            label10.TabIndex = 2;
+            label10.Text = "-23% ";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Tajawal", 16.1999989F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label11.Location = new Point(259, 50);
+            label11.Name = "label11";
+            label11.Size = new Size(63, 40);
+            label11.TabIndex = 1;
+            label11.Text = "10%";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Tajawal Medium", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label12.ForeColor = Color.Gray;
+            label12.Location = new Point(218, 17);
+            label12.Name = "label12";
+            label12.Size = new Size(130, 23);
+            label12.TabIndex = 0;
+            label12.Text = "معدل الاشغال";
+            // 
+            // Dashboard
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoScroll = true;
+            BackColor = Color.FromArgb(248, 250, 252);
+            ClientSize = new Size(1004, 636);
+            Controls.Add(panel4);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Dashboard";
+            Text = "Dashboard";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Panel panel2;
+        private Panel panel3;
+        private PictureBox pictureBox3;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Panel panel4;
+        private PictureBox pictureBox4;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+    }
+}
--- ActiveSpaceSystem/Program.cs
+++ ActiveSpaceSystem/Program.cs
@@ -1,4 +1,4 @@
-using ActiveSpaceSystem.Forms;
+using ActiveSpaceSystem.Forms.SideForms;
 namespace ActiveSpaceSystem
 {
     internal static class Program
@@ -12,7 +12,7 @@ namespace ActiveSpaceSystem
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new LoginForm());
+            Application.Run(new Dashboard());
         }
     }
 }

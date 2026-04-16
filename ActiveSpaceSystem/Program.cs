365c90e3e257fed94b0c68b4cc070c4bc8bb4582
3bdo-Kashoun <abdlmhimn.boro@gmail.com>
2026-04-16 18:04:40 +0200

    gg

--- ActiveSpaceSystem/Program.cs
+++ ActiveSpaceSystem/Program.cs
@@ -13,11 +13,8 @@ namespace ActiveSpaceSystem
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-
             Application.Run(new MainForm());
-
-            
-
+            //Application.Run(new Dashboard());
         }
     }
 }
